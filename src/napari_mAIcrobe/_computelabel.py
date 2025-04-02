f8127e384d5cf80fde2d3e4643b92008183d2d63
António Brito <50997716+antmsbrito@users.noreply.github.com>
2025-04-02 11:41:21 +0100

    Added support for a Unet segmentation model trained in zerocost

--- src/napari_mAIcrobe/_computelabel.py
+++ src/napari_mAIcrobe/_computelabel.py
@@ -16,6 +16,91 @@ from magicgui.widgets import Container, create_widget, SpinBox, ComboBox, FileEd
 from qtpy.QtCore import Qt
 from qtpy import QtWidgets
 
+from tensorflow.keras.models import load_model
+import numpy as np
+
+from math import ceil
+
+from skimage.morphology import binary_erosion
+from skimage.segmentation import watershed
+from scipy.ndimage import label as lbl
+
+
+# Normalization functions from Martin Weigert
+def normalizePercentile(x, pmin=1, pmax=99.8, axis=None, clip=False, eps=1e-20, dtype=np.float32):
+    """This function is adapted from Martin Weigert"""
+    """Percentile-based image normalization."""
+
+    mi = np.percentile(x,pmin,axis=axis,keepdims=True)
+    ma = np.percentile(x,pmax,axis=axis,keepdims=True)
+    return normalize_mi_ma(x, mi, ma, clip=clip, eps=eps, dtype=dtype)
+
+
+def normalize_mi_ma(x, mi, ma, clip=False, eps=1e-20, dtype=np.float32):#dtype=np.float32
+    """This function is adapted from Martin Weigert"""
+    if dtype is not None:
+        x   = x.astype(dtype,copy=False)
+        mi  = dtype(mi) if np.isscalar(mi) else mi.astype(dtype,copy=False)
+        ma  = dtype(ma) if np.isscalar(ma) else ma.astype(dtype,copy=False)
+        eps = dtype(eps)
+
+    try:
+        import numexpr
+        x = numexpr.evaluate("(x - mi) / ( ma - mi + eps )")
+    except ImportError:
+        x =                   (x - mi) / ( ma - mi + eps )
+
+    if clip:
+        x = np.clip(x,0,1)
+
+    return x
+
+def predict_as_tiles(img, model):
+
+  # Read the data in and normalize
+  Image_raw = normalizePercentile(img)
+
+  # Get the patch size from the input layer of the model
+  patch_size = model.layers[0].output_shape[0][1:3]
+
+  # Pad the image with zeros if any of its dimensions is smaller than the patch size
+  if Image_raw.shape[0] < patch_size[0] or Image_raw.shape[1] < patch_size[1]:
+    Image = np.zeros((max(Image_raw.shape[0], patch_size[0]), max(Image_raw.shape[1], patch_size[1])))
+    Image[0:Image_raw.shape[0], 0: Image_raw.shape[1]] = Image_raw
+  else:
+    Image = Image_raw
+
+  # Calculate the number of patches in each dimension
+  n_patch_in_width = ceil(Image.shape[0]/patch_size[0])
+  n_patch_in_height = ceil(Image.shape[1]/patch_size[1])
+
+  prediction = np.zeros(Image.shape, dtype = 'uint8')
+
+  for x in range(n_patch_in_width):
+    for y in range(n_patch_in_height):
+      xi = patch_size[0]*x
+      yi = patch_size[1]*y
+
+      # If the patch exceeds the edge of the image shift it back
+      if xi+patch_size[0] >= Image.shape[0]:
+        xi = Image.shape[0]-patch_size[0]
+
+      if yi+patch_size[1] >= Image.shape[1]:
+        yi = Image.shape[1]-patch_size[1]
+
+      # Extract and reshape the patch
+      patch = Image[xi:xi+patch_size[0], yi:yi+patch_size[1]]
+      patch = np.reshape(patch,patch.shape+(1,))
+      patch = np.reshape(patch,(1,)+patch.shape)
+
+      # Get the prediction from the patch and paste it in the prediction in the right place
+      predicted_patch = model.predict(patch, batch_size = 1)
+
+      prediction[xi:xi+patch_size[0], yi:yi+patch_size[1]] = (np.argmax(np.squeeze(predicted_patch), axis = -1)).astype(np.uint8)
+
+
+  return prediction[0:Image_raw.shape[0], 0: Image_raw.shape[1]]
+
 
 
 class compute_label(Container):
@@ -40,7 +125,7 @@ class compute_label(Container):
 
         self._titlemasklabel = Label(value='Parameters for Mask computation')
         self._titlemasklabel.native.setAlignment(Qt.AlignCenter)
-        self._titlemasklabel.native.setStyleSheet("background-color: rgb(037, 041, 049); border: 1px solid black;")
+        self._titlemasklabel.native.setStyleSheet("background-color: rgb(037, 041, 049); border: 1px solid rgb(059, 068, 077);")
         self._titlemasklabel.native.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)        
         
         self._placeholder = Label(value='...')
@@ -53,7 +138,7 @@ class compute_label(Container):
 
         # WATERSHED ALGORITHM
         self._titlewatershedlabel = Label(value='Parameters for Watershed Algorithm')
-        self._titlewatershedlabel.native.setStyleSheet("background-color: rgb(037, 041, 049); border: 1px solid black;")
+        self._titlewatershedlabel.native.setStyleSheet("background-color: rgb(037, 041, 049); border: 1px solid rgb(059, 068, 077);")
         self._titlewatershedlabel.native.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
 
         self._titlewatershedlabel.native.setAlignment(Qt.AlignCenter)
@@ -108,7 +193,19 @@ class compute_label(Container):
         _pars = {'peak_min_distance_from_edge':self._peak_min_distance_from_edge.value,'peak_min_distance':self._peak_min_distance.value,'peak_min_height':self._peak_min_height.value,'max_peaks':self._max_peaks.value}
 
         if _algorithm == "Unet":
-            return None
+            model = load_model(self._path2unet.value)
+            prediction = predict_as_tiles(_baseimg.data, model) 
+
+            mask = prediction>0
+            #edges = prediction==1
+            insides = prediction==2
+            for _ in range(3):
+                insides = binary_erosion(insides)
+            insides = insides.astype(np.uint16)
+            insides, _ = lbl(insides)
+
+            labels = watershed(~mask,markers=insides,mask=mask)
+
         else:
             mask = mask_computation(base_image=_baseimg.data,algorithm=_algorithm,blocksize=_LAblocksize,
                             offset=_LAoffset,closing=_binary_closing,dilation=_binary_dilation,fillholes=_binary_fillholes)
@@ -116,16 +213,18 @@ class compute_label(Container):
             seg_man = SegmentsManager()
             seg_man.compute_segments(_pars, mask)
 
-            # add mask to viewer
-            self._viewer.add_labels(mask,name='Mask')
-            # add labelimg to viewer
-            self._viewer.add_labels(seg_man.labels,name='Labels')
+            labels = seg_man.labels
 
-            if _autoalign:
-                aligned_fluor_1 = mask_alignment(mask, _fluor1.data)
-                aligned_fluor_2 = mask_alignment(mask, _fluor2.data)
+        # add mask to viewer
+        self._viewer.add_labels(mask,name='Mask')
+        # add labelimg to viewer
+        self._viewer.add_labels(labels,name='Labels')
 
-                self._viewer.layers[_fluor1.name].data = aligned_fluor_1
-                self._viewer.layers[_fluor2.name].data = aligned_fluor_2
+        if _autoalign:
+            aligned_fluor_1 = mask_alignment(mask, _fluor1.data)
+            aligned_fluor_2 = mask_alignment(mask, _fluor2.data)
 
-            
+            self._viewer.layers[_fluor1.name].data = aligned_fluor_1
+            self._viewer.layers[_fluor2.name].data = aligned_fluor_2
+
+        
